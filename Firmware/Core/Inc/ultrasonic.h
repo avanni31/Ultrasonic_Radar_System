d29d343bd81247e3d926af3f28ecfe2cdb87bf5f
avanni31 <avanni05@gmail.com>
2026-06-24 15:17:16 +0530

    Update ultrasonic.h

--- Firmware/Core/Inc/ultrasonic.h
+++ Firmware/Core/Inc/ultrasonic.h
@@ -1,1 +1,19 @@
+/**
+  ******************************************************************************
+  * @file           : ultrasonic.h
+  * @brief          : Header for HC-SR04 ultrasonic sensor driver
+  ******************************************************************************
+  */
+#ifndef __ULTRASONIC_H
+#define __ULTRASONIC_H
+#include "stm32f3xx_hal.h"
+/* Function Prototypes */
+void DWT_Init(void);
+void DWT_Delay_us(uint32_t microseconds);
+void Ultrasonic_Init(TIM_HandleTypeDef *htim);
+void Ultrasonic_Trigger(void);
+float Ultrasonic_GetDistance(void);
+/* Interrupt Callback Handler to be invoked by HAL TIM Input Capture Callback */
+void Ultrasonic_CaptureCallback(TIM_HandleTypeDef *htim);
+#endif /* __ULTRASONIC_H */
 
