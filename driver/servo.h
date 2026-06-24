c831a2ac701123b7ba066f873dd7a4fe7665eb2d
avanni31 <avanni05@gmail.com>
2026-06-24 15:18:20 +0530

    Add header file for MG90/SG90 servo driver

--- driver/servo.h
+++ driver/servo.h
@@ -1,1 +1,14 @@
+/**
+  ******************************************************************************
+  * @file           : servo.h
+  * @brief          : Header for MG90/SG90 PWM Servo Motor driver
+  ******************************************************************************
+  */
+#ifndef __SERVO_H
+#define __SERVO_H
+#include "stm32f3xx_hal.h"
+/* Function Prototypes */
+void Servo_Init(TIM_HandleTypeDef *htim, uint32_t channel);
+void Servo_SetAngle(uint8_t angle);
+#endif /* __SERVO_H */
 
